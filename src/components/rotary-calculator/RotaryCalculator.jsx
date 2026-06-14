22928bf65e5c9a86431889dad50c85d15332ebd8
Aidan Wang <aidan79225@gmail.com>
2026-06-14 10:04:50 +0800

    feat: scaffold rotary-calculator page + tools hub entry

---
+++ src/components/rotary-calculator/RotaryCalculator.jsx
@@ -0,0 +1,9 @@
+import './rotary-calculator.css';
+
+export default function RotaryCalculator() {
+  return (
+    <div className="rc-stage">
+      <p>Rotary calculator coming soon.</p>
+    </div>
+  );
+}
